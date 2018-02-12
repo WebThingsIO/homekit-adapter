944cd842b3444df8ae70af58308061d9dd66465a
Mike Stegeman <mrstegeman@gmail.com>
2018-02-12 10:42:46 -0700

    Initial adapter stub.
    Pairing is currently failing, thus I can't yet flesh out device or
    property definitions properly.

---
+++ package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf node_modules
+yarn install --production
+rm -f SHA256SUMS
+sha256sum package.json *.js LICENSE > SHA256SUMS
+find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
+TARFILE=$(npm pack)
+tar xzf "${TARFILE}"
+cp -r node_modules ./package
+tar czf "${TARFILE}" package
+rm -rf package
