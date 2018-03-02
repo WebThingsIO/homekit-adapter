bc93987e5b8613fe4a5e9db17e5589d7187b0867
Mike Stegeman <mrstegeman@gmail.com>
2018-03-02 07:27:12 -0700

    Update packaging script, bump API to 2.

--- package.sh
+++ package.sh
@@ -4,15 +4,28 @@ set -e
 
 version=$(grep version package.json | cut -d: -f2 | cut -d\" -f2)
 
-rm -f SHA256SUMS
-sha256sum *.py pkg/*.py LICENSE > SHA256SUMS
-rm -rf lib
-mkdir lib
-SODIUM_INSTALL=system pip3 install -r requirements.txt -t lib --prefix "" --no-deps
-
+# Clean up from previous releases
 rm -rf *.tgz package
+rm -f SHA256SUMS
+rm -rf lib
+
+# Prep new package
+mkdir lib
 mkdir package
-cp -r lib pkg LICENSE SHA256SUMS package.json *.py package/
+
+# Pull down Python dependencies
+pip3 install -r requirements.txt -t lib --prefix ""
+
+# Put package together
+cp -r lib pkg LICENSE package.json *.py package/
 find package -type f -name '*.pyc' -delete
 find package -type d -empty -delete
+
+# Generate checksums
+cd package
+sha256sum *.py pkg/*.py LICENSE > SHA256SUMS
+find lib -type f -exec sha256sum {} \; >> SHA256SUMS
+cd -
+
+# Make the tarball
 tar czf "homekit-adapter-${version}.tgz" package
