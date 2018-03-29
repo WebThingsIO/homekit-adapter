78c1f24ca3837a9bbf2b16e208a854139cdf652f
Mike Stegeman <mrstegeman@gmail.com>
2018-03-29 09:01:34 -0600

    Bootstrap dependencies before running.
    Instead of building the package for every platform/arch/python
    combination, just install the dependencies before running.

--- package.sh
+++ package.sh
@@ -7,24 +7,16 @@ version=$(grep version package.json | cut -d: -f2 | cut -d\" -f2)
 # Clean up from previous releases
 rm -rf *.tgz package
 rm -f SHA256SUMS
-rm -rf lib
-
-# Prep new package
-mkdir lib
-mkdir package
-
-# Pull down Python dependencies
-pip3 install -r requirements.txt -t lib --prefix ""
 
 # Put package together
-cp -r lib pkg LICENSE package.json *.py package/
+mkdir package
+cp -r pkg LICENSE package.json *.py requirements.txt setup.cfg package/
 find package -type f -name '*.pyc' -delete
 find package -type d -empty -delete
 
 # Generate checksums
 cd package
-sha256sum *.py pkg/*.py LICENSE > SHA256SUMS
-find lib -type f -exec sha256sum {} \; >> SHA256SUMS
+sha256sum *.py pkg/*.py LICENSE requirements.txt setup.cfg > SHA256SUMS
 cd -
 
 # Make the tarball
