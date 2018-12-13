f9161b2338a99eb9191fa5896cd09542256cdb2f
Michael Stegeman <michael@stegeman.me>
2018-12-13 10:45:27 -0700

    Add additional debug output.

--- package.sh
+++ package.sh
@@ -16,7 +16,7 @@ else
 fi
 
 rm -f SHA256SUMS
-sha256sum package.json *.js LICENSE > SHA256SUMS
+sha256sum package.json *.js lib/*.js LICENSE > SHA256SUMS
 find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
 TARFILE="$(npm pack)"
 tar xzf ${TARFILE}
