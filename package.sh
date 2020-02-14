c94d2896ff057e67f77c65e318a63e60dc16bbfa
Michael Stegeman <michael@stegeman.me>
2020-02-14 09:21:31 -0900

    Update packaging script.

--- package.sh
+++ package.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -e
 
 rm -rf node_modules
+
 if [ -z "${ADDON_ARCH}" ]; then
   TARFILE_SUFFIX=
 else
   NODE_VERSION="$(node --version)"
   TARFILE_SUFFIX="-${ADDON_ARCH}-${NODE_VERSION/\.*/}"
 fi
+
 # For openwrt-linux-arm and linux-arm we need to cross compile.
 if [[ "${ADDON_ARCH}" =~ "linux-arm" ]]; then
   # We assume that CC and CXX are pointing to the cross compilers
@@ -16,15 +18,18 @@ else
   npm install --production
 fi
 
-rm -f SHA256SUMS
-sha256sum manifest.json package.json *.js lib/*.js LICENSE README.md > SHA256SUMS
-rm -rf node_modules/.bin
-find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
-TARFILE="$(npm pack)"
+shasum --algorithm 256 manifest.json package.json *.js lib/*.js LICENSE README.md > SHA256SUMS
+
+find node_modules \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+
+TARFILE=`npm pack`
+
 tar xzf ${TARFILE}
 rm ${TARFILE}
 TARFILE_ARCH="${TARFILE/.tgz/${TARFILE_SUFFIX}.tgz}"
 cp -r node_modules ./package
 tar czf ${TARFILE_ARCH} package
-rm -rf package
-echo "Created ${TARFILE_ARCH}"
+
+shasum --algorithm 256 ${TARFILE_ARCH} > ${TARFILE_ARCH}.sha256sum
+
+rm -rf SHA256SUMS package
